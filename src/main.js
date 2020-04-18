22151d5d5cef1f93f333c21bb4195d3437969cf1
jdvolk <justin.d.volk@gmail.com>
2020-04-18 16:19:53 -0600

    game loop in place

---
+++ src/main.js
@@ -0,0 +1,35 @@
+
+function playGame() {
+  debugger
+  var player1 = new Player("justin", "x", undefined);
+  var player2 = new Player("trevor", "o", undefined);
+  var game1 = new Game(player1, player2);
+  for(;;) {
+    // player choses spot
+    var moveResult = game1.makeMove(5 ,player1);
+    var moveResult = game1.makeMove(7, player2);
+    var moveResult = game1.makeMove(300, player1);
+    var moveResult = game1.makeMove(-7, player2);
+    var moveResult = game1.makeMove(79, player1);
+    var moveResult = game1.makeMove("litty", player2);
+    var moveResult = game1.makeMove(5, player2);
+    var moveResult = game1.makeMove(3, player1);
+    // check spot taken
+    // if taken ask again
+    if (moveResult === false) {
+      // ask again
+    }
+    // if not taken mark spot
+    // check board for win conditions
+    if (game1.isDone() === true) {
+      // save wins
+      // reset board
+      // update wins with number of boards in wins.length
+      break;
+    }
+    break;
+    // remove break after testing
+  }
+
+}
+
