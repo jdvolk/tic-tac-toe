f26622d497e863db95966fb47e502ea71eb853fe
jdvolk <justin.d.volk@gmail.com>
2020-04-21 17:36:22 -0600

    add retrieve win data from locla storage with a function on line 120

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ var gameBoardSection = document.querySelector('.game-board');
 var player1 = new Player("justin", "x", undefined);
 var player2 = new Player("trevor", "o", undefined);
 var game = new Game(player1, player2);
-window.onload = drawWins();
+window.onload = retrieveWinData();
 // event listeners
 topBannerSection.addEventListener('click', updateTopBanner);
 gameSection.addEventListener('click', (function(){}))
@@ -46,7 +46,6 @@ gameBoardSection.addEventListener('click', (function(event) {
  function drawWins() {
   leftWinSectionUl.innerHTML = "";
   rightWinSectionUl.innerHTML = "";
-   debugger
    for (var i = 0; i < player1.wins.length; i++) {
     var winHTML = createWinHtml(player1.wins[i]);
     leftWinSectionUl.innerHTML += winHTML;
@@ -78,35 +77,35 @@ function updateTopBanner() {
 }
 // functions
 function playGame() {
-  debugger
-  var player1 = new Player("justin", "x", undefined);
-  var player2 = new Player("trevor", "o", undefined);
-  var game1 = new Game(player1, player2);
-  for(;;) {
+  // debugger
+  // var player1 = new Player("justin", "x", undefined);
+  // var player2 = new Player("trevor", "o", undefined);
+  // var game1 = new Game(player1, player2);
+  // for(;;) {
     // player choses spot
-    var moveResult = game1.makeMove(0 ,player1);
-    var moveResult = game1.makeMove(1, player2);
-    var moveResult = game1.makeMove(2, player1);
-    var moveResult = game1.makeMove(3, player2);
-    var moveResult = game1.makeMove(5, player1);
-    var moveResult = game1.makeMove(4, player2);
-    var moveResult = game1.makeMove(7, player1);
-    var moveResult = game1.makeMove(6, player2);
-    var moveResult = game1.makeMove(8, player1);
-    // check spot taken
-    // if taken ask again
-    if (moveResult === false) {
-      // ask again
-    }
-    if (game1.winner !== undefined) {
-      console.log(game1.winner)
-    } else if (game1.isDraw()) {
-      console.log("doBetter")
-    }
+    // var moveResult = game1.makeMove(0 ,player1);
+    // var moveResult = game1.makeMove(1, player2);
+    // var moveResult = game1.makeMove(2, player1);
+    // var moveResult = game1.makeMove(3, player2);
+    // var moveResult = game1.makeMove(5, player1);
+    // var moveResult = game1.makeMove(4, player2);
+    // var moveResult = game1.makeMove(7, player1);
+    // var moveResult = game1.makeMove(6, player2);
+    // var moveResult = game1.makeMove(8, player1);
+      // // check spot taken
+      // if taken ask again
+    // if (moveResult === false) {
+        //   // ask again
+    // }
+    // if (game1.winner !== undefined) {
+    //   console.log(game1.winner)
+    // } else if (game1.isDraw()) {
+    //   console.log("doBetter")
+    // }
     // if not taken mark spot
     // check board for win conditions
-    game1.resetBoard();
-    break;
+    // game1.resetBoard();
+    // break;
     // remove break after testing
   }
 
@@ -118,3 +117,15 @@ function generateTokenHTML(token) {
   }
   return `<img class="token" src="assets/${token}-token.svg" />`;
 }
+function retrieveWinData() {
+  player1.retrieveWins();
+  player2.retrieveWins();
+  drawWins();
+}
+
+// function retrieveFromStorage() {
+//   savedIdeasArray = JSON.parse(localStorage.getItem("savedIdeasArray")) || [];
+
+  //   var reinstatedIdea = new Idea(currentIdea.id, currentIdea.title, currentIdea.body, currentIdea.star);
+  //   savedIdeasArray[i] = reinstatedIdea;
+  // updatePageHtml(savedIdeasArray);
