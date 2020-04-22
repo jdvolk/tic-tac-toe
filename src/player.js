71840a821447de4ccaea868f745925a7738217ca
jdvolk <justin.d.volk@gmail.com>
2020-04-21 20:16:23 -0600

    finish functionality for scrolling win display/ clean commented code/ fixed bug on grid

--- src/player.js
+++ src/player.js
@@ -5,9 +5,7 @@ class Player {
     this.token = token;
     this.wins = wins || [];
   }
-  // methods must include but not limited to:
   saveWinsToStorage() {
-    // this.wins.push(board);
     localStorage.setItem(`player_${this.id}_wins`, JSON.stringify(this.wins));
   }
   retrieveWins() {
